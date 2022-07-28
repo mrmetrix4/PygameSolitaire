200175f4d1cf4f58ec6d992bce4b1c81a548f301
Adar Arbiv <mrmetrix4@gmail.com>
2022-07-28 07:10:05 +0300

    Added drawing capabilities.

--- structures/column.py
+++ structures/column.py
@@ -1,5 +1,6 @@
 import pygame
 
+from constants import locations
 from structures.card import Card
 
 
@@ -10,4 +11,14 @@ class Column:
         self.cards: list[Card] = cards
 
     def draw_column(self, screen):
-        pass
+        center_x = locations.ROW_ZERO_CENTER_X[self.idx]
+        col_len = len(self.cards)
+        for i, c in enumerate(self.cards):
+            center_y = 350 + i * 30
+            if i == col_len - 1:
+                c.draw(screen, center=(center_x, center_y))
+                continue
+            back_rect = self.card_back_sprite.get_rect(
+                center=(center_x, center_y)
+            )
+            screen.blit(self.card_back_sprite, back_rect)
